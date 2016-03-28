1d089cbd52878d11057511119671206a4f397f6b
Shinwoo Kim <cinoo.kim@samsung.com>
2016-03-28 21:48:31 +0900

    Initial draft: checked module loaded properly with modified e-tizen-data (https://review.tizen.org/gerrit/#/c/63914/)
    Change-Id: I4878351c44a72394c2e90a710c4b3de503449048

---
+++ src/e_mod_main.c
@@ -0,0 +1,44 @@
+#include "e.h"
+#include "eina_log.h"
+#include "e_mod_main.h"
+
+#undef DBG
+int _log_dom = -1;
+#define DBG(...)  do EINA_LOG_DOM_DBG(_log_dom, __VA_ARGS__); while(0)
+
+EAPI E_Module_Api e_modapi =
+{
+   E_MODULE_API_VERSION,
+   "Screen Reader Module of Window Manager"
+};
+
+EAPI void *
+e_modapi_init(E_Module *m)
+{
+   _log_dom = eina_log_domain_register("e_screen_reader", EINA_COLOR_YELLOW);
+   if (_log_dom < 0)
+     {
+        DBG("Failed @ eina_log_domain_register()..!");
+        return NULL;
+     }
+
+   //Start from here
+   //DBG("ecore evas: %p", e_comp->ee);
+
+   return m;
+}
+
+EAPI int
+e_modapi_shutdown(E_Module *m EINA_UNUSED)
+{
+   eina_log_domain_unregister(_log_dom);
+
+   return 1;
+}
+
+EAPI int
+e_modapi_save(E_Module *m EINA_UNUSED)
+{
+   /* Do Something */
+   return 1;
+}
